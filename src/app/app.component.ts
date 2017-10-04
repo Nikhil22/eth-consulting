d54b032a255b8536552afe8952eb9b1dceb6d175
Rowan de Haas <mechonomics@gmail.com>
2017-10-04 15:34:42 -0400

    Remove body ml3 class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  host: { class: 'ml3' }
+  host: { class: '' }
 })
 export class AppComponent {
   companyName = 'Test';
